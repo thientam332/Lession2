fbab69297f4fba47689c6cce3160b228930741ef
thientam332 <76957177+thientam332@users.noreply.github.com>
2021-12-31 13:27:09 +0700

    update

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require 'Core/Database.php';
 require_once 'models/MasterModel.php';
 require_once 'models/ListModel.php';
+//Định nghĩa controller từ request từ url
 if (isset($_GET['controller'], $_GET['action'])){
     $controller = $_GET['controller'];
     $action= $_GET['action'];}
